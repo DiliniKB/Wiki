ef7fdc81f7f56fb84d20505dce4006a46943b9fc
DiliniKB <84224505+DiliniKB@users.noreply.github.com>
2022-08-28 02:03:49 +0530

    navigations

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import markdown2
 
 from . import util
 
@@ -6,5 +7,11 @@ from . import util
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def page(request, page):
+    return render(request, "encyclopedia/page.html", {
+        "title": {page},
+        "content": markdown2.markdown(util.get_entry(page))
     })
 
